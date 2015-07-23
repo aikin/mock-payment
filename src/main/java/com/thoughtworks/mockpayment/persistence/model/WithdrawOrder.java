e73f96e24dec8550b2a3fd7c98a2ec0b6c1efe2e
aikin <me@ulaijn.com>
2015-07-23 16:31:47 +0800

    aikin: remove idCardNo filed on withdraw order

--- src/main/java/com/thoughtworks/mockpayment/persistence/model/WithdrawOrder.java
+++ src/main/java/com/thoughtworks/mockpayment/persistence/model/WithdrawOrder.java
@@ -15,7 +15,6 @@ public class WithdrawOrder {
     private String bankCardNo;
     private String bankName;
     private String userName;
-    private String idCardNo;
     private String amount;
     private WithdrawStatus withdrawStatus;
     private String responseCode;
@@ -28,7 +27,6 @@ public class WithdrawOrder {
                          String customerId,
                          String orderId,
                          String userName,
-                         String idCardNo,
                          String bankCode,
                          String bankCardNo,
                          String bankName,
@@ -41,10 +39,121 @@ public class WithdrawOrder {
         this.bankCardNo = bankCardNo;
         this.bankName = bankName;
         this.userName = userName;
-        this.idCardNo = idCardNo;
         this.amount = amount;
         this.createdAt = DateTime.now().toDate();
         this.withdrawStatus = WithdrawStatus.PROCESSING;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBatchNo() {
+        return batchNo;
+    }
+
+    public void setBatchNo(String batchNo) {
+        this.batchNo = batchNo;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getBankCode() {
+        return bankCode;
+    }
+
+    public void setBankCode(String bankCode) {
+        this.bankCode = bankCode;
+    }
+
+    public String getBankCardNo() {
+        return bankCardNo;
+    }
+
+    public void setBankCardNo(String bankCardNo) {
+        this.bankCardNo = bankCardNo;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public WithdrawStatus getWithdrawStatus() {
+        return withdrawStatus;
+    }
+
+    public void setWithdrawStatus(WithdrawStatus withdrawStatus) {
+        this.withdrawStatus = withdrawStatus;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getWithdrawMessage() {
+        return withdrawMessage;
+    }
+
+    public void setWithdrawMessage(String withdrawMessage) {
+        this.withdrawMessage = withdrawMessage;
+    }
+
+    public Date getWithdrawAt() {
+        return withdrawAt;
+    }
+
+    public void setWithdrawAt(Date withdrawAt) {
+        this.withdrawAt = withdrawAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
     public static enum WithdrawStatus {
---
+++ src/main/java/com/thoughtworks/mockpayment/service/DefaultWithdrawService.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.mockpayment.service;
+
+/**
+ * Created by aikin on 7/23/15.
+ */
+public class DefaultWithdrawService {
+}
