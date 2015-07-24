0aaeceeaa9559422b7a804537cbd4ce30c1cc202
aikin <me@ulaijn.com>
2015-07-24 10:28:55 +0800

    aikin: complete default withdraw service

--- src/main/java/com/thoughtworks/mockpayment/service/DefaultWithdrawService.java
+++ src/main/java/com/thoughtworks/mockpayment/service/DefaultWithdrawService.java
@@ -1,38 +1,66 @@
 package com.thoughtworks.mockpayment.service;
 
 
+import com.google.inject.Inject;
 import com.thoughtworks.mockpayment.entity.withdraw.BankCardNoAndResponseCodeMap;
 import com.thoughtworks.mockpayment.entity.withdraw.WithdrawResponseCode;
+import com.thoughtworks.mockpayment.entity.withdraw.WithdrawResult;
+import com.thoughtworks.mockpayment.persistence.mapper.WithdrawOrderMapper;
 import com.thoughtworks.mockpayment.persistence.model.WithdrawOrder;
+import com.thoughtworks.mockpayment.util.Json;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 import java.util.Map;
 
 public class DefaultWithdrawService implements WithdrawService {
 
-
     private static final Logger logger = LoggerFactory.getLogger(DefaultWithdrawService.class);
+    private static final int WAIT_RESPONSE_WITHDRAW_RESULT = 1000;
+
+    @Inject
+    private WithdrawOrderMapper withdrawOrderMapper;
 
     @Override
     public String handleWithdrawRequest(Map<String, String> withdrawRequest) {
 
         logger.debug("*** in handle withdraw request ***" + withdrawRequest);
 
-
         WithdrawOrder withdrawOrder = new WithdrawOrder(withdrawRequest);
+        withdrawOrderMapper.insertNewOrder(withdrawOrder);
 
-        String responseCode =  BankCardNoAndResponseCodeMap.fetchStatusCodeByBankCardNo(withdrawRequest.getBankCardNo());
+        WithdrawResult withdrawResult = this.generateWithdrawResult(withdrawOrder);
+        return Json.toJSON(withdrawResult);
+    }
+
+    private WithdrawResult generateWithdrawResult(WithdrawOrder withdrawOrder) {
+
+        String responseCode =  BankCardNoAndResponseCodeMap.fetchStatusCodeByBankCardNo(withdrawOrder.getBankCardNo());
         WithdrawResponseCode withdrawResponseCode = WithdrawResponseCode.codeOf(responseCode);
 
-
         if (withdrawResponseCode == null) {
-            logger.debug("*** input bankCardNo not match status deposits response code ***" + withdrawOrder.getBankCardNo());
+            logger.debug("*** input bankCardNo not match status withdraw response code ***" + withdrawOrder.getBankCardNo());
             withdrawResponseCode = WithdrawResponseCode.SUCCESS;
         }
 
+        WithdrawResult withdrawResult = new WithdrawResult(withdrawOrder, withdrawResponseCode);
+        this.handleWithdrawOrder(withdrawResult, withdrawResponseCode);
+        return withdrawResult;
+    }
 
-        return null;
+    private void handleWithdrawOrder(WithdrawResult withdrawResult, WithdrawResponseCode withdrawResponseCode) {
+        try {
+            Thread.sleep(WAIT_RESPONSE_WITHDRAW_RESULT);
+        } catch (InterruptedException e) {
+            logger.error(e.getLocalizedMessage(), e);
+        }
+
+        this.withdrawOrderMapper.updateWithdrawStatus(
+            withdrawResult.getWithdrawFlowId(),
+            withdrawResult.getWithdrawAt(),
+            withdrawResponseCode.getStatus(),
+            withdrawResponseCode.getCode(),
+            withdrawResponseCode.getDescription()
+        );
     }
 }
